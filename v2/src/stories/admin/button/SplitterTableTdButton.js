b75633c7280c86cd878d900b0cfca1d93b3e1bf5
unknown <amywine0701@gmail.com>
2026-01-12 16:49:46 +0900

    커밋 테스트

--- v2/src/stories/admin/button/SplitterTableTdButton.js
+++
@@ -1,7 +0,0 @@
-import './splitterButton.css';
-
-export function SplitterTableTdButton({ label, disabled = false }) {
-  return `
-    <button type="button" class="btn_input" ${disabled ? 'disabled' : ''}><span>${label}</span></button>
-  `;
-}
--- v2/src/stories/admin/button/SplitterTableTopButton.js
+++
@@ -1,9 +0,0 @@
-import './splitterButton.css';
-
-export function SplitterTableTopButton({ label, disabled = false }) {
-  return `
-    <div class="table_btn only-btn-right">
-      <button type="button" class="btn_inner" ${disabled ? 'disabled' : ''}><span>${label}</span></button>
-    </div>
-  `;
-}
